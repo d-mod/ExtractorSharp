d139ff7052497e70f7b96be1fa0a46f05759d06c
provefar <provefars@gmail.com>
2020-05-03 22:51:21 +0800

    修复测试的合并错误

--- ExtractorSharp.UnitTest/Command/UnitTest1.cs
+++ ExtractorSharp.UnitTest/Command/UnitTest1.cs
@@ -12,55 +12,6 @@ namespace ExtractorSharp.UnitTest.Command
     [TestClass]
     public class UnitTest1
     {
-        static UnitTest1()
-        {
-            Handler.Regisity(ImgVersion.Other, typeof(OtherHandler));
-            Handler.Regisity(ImgVersion.Ver1, typeof(FirstHandler));
-            Handler.Regisity(ImgVersion.Ver2, typeof(SecondHandler));
-            Handler.Regisity(ImgVersion.Ver4, typeof(FourthHandler));
-            Handler.Regisity(ImgVersion.Ver5, typeof(FifthHandler));
-            Handler.Regisity(ImgVersion.Ver6, typeof(SixthHandler));
-        }
-
-
-        /// <summary>
-        ///     测试IMG保存
-        /// </summary>
-        [TestMethod]
-        public void TestSaveImg()
-        {
-            var list = NpkCoder.Load(@"D:\地下城与勇士\ImagePacks2\sprite_character_swordman_equipment_avatar_coat.NPK");
-            Assert.IsTrue(list.Count > 0);
-            var img = list[0];
-            Assert.IsNotNull(img);
-            var target = @"d:\test\v6\01.img";
-            img.Save(target);
-            Assert.IsTrue(File.Exists(target));
-            list = NpkCoder.Load(target);
-            Assert.IsTrue(list.Count > 0);
-            img = list[0];
-            Assert.IsNotNull(img);
-        }
-
-
-        [TestMethod]
-        public void TestSaveImage()
-        {
-            var list = NpkCoder.Load(@"D:\test\v6\01.img");
-            Assert.IsTrue(list.Count > 0);
-            var img = list[0];
-            Assert.IsNotNull(img);
-            var dir = @"D:\test\v6\image\";
-            if (Directory.Exists(dir))
-            {
-                Directory.Delete(dir, true);
-                Directory.CreateDirectory(dir);
-            }
-            img.List.ForEach(e => e.Picture.Save(dir + e.Index + ".png"));
-            Assert.IsTrue(Directory.GetFiles(dir, "*.png").Length == img.Count);
-        }
-
-
         [TestMethod]
         public void TestInvoke()
         {
@@ -74,16 +25,16 @@ namespace ExtractorSharp.UnitTest.Command
 
             Assert.AreEqual(commandParser.InvokeToken("1;"), "1");
             Assert.AreEqual(commandParser.InvokeToken("1|toInt;"), 1);
-            Assert.IsTrue(((commandParser.InvokeToken("1|toArray;") as string[]) ?? Array.Empty<string>()).SequenceEqual(new[] { "1" }));
-            Assert.IsTrue(((commandParser.InvokeToken("1|toArray|toInt;") as int[]) ?? Array.Empty<int>()).SequenceEqual(new[] { 1 }));
+            Assert.IsTrue(((commandParser.InvokeToken("1|toArray;") as string[]) ?? new string[]{}).SequenceEqual(new[] { "1" }));
+            Assert.IsTrue(((commandParser.InvokeToken("1|toArray|toInt;") as int[]) ?? new int[]{}).SequenceEqual(new[] { 1 }));
 
-            Assert.IsTrue(((commandParser.InvokeToken("1,2,3|toArray;") as string[]) ?? Array.Empty<string>()).SequenceEqual(new[] { "1", "2", "3" }));
-            Assert.IsTrue(((commandParser.InvokeToken("1,2,3|toArray|toInt;") as int[]) ?? Array.Empty<int>()).SequenceEqual(new[] { 1, 2, 3 }));
+            Assert.IsTrue(((commandParser.InvokeToken("1,2,3|toArray;") as string[]) ?? new string[] { }).SequenceEqual(new[] { "1", "2", "3" }));
+            Assert.IsTrue(((commandParser.InvokeToken("1,2,3|toArray|toInt;") as int[]) ?? new int[]{}).SequenceEqual(new[] { 1, 2, 3 }));
             commandParser.InvokeToken("1,2,3|toArray|toInt|asVar|a;");
-            Assert.IsTrue((commandParser.InvokeToken("|useVar|a;") as int[] ?? Array.Empty<int>()).SequenceEqual(new[] { 1, 2, 3 }));
+            Assert.IsTrue((commandParser.InvokeToken("|useVar|a;") as int[] ?? new int[]{}).SequenceEqual(new[] { 1, 2, 3 }));
 
             commandParser.InvokeToken("1,2,3|toArray|asVar|a;");
-            Assert.IsTrue((commandParser.InvokeToken("|useVar|a|toInt;") as int[] ?? Array.Empty<int>()).SequenceEqual(new[] { 1, 2, 3 }));
+            Assert.IsTrue((commandParser.InvokeToken("|useVar|a|toInt;") as int[] ?? new int[]{}).SequenceEqual(new[] { 1, 2, 3 }));
 
             Assert.AreEqual(commandParser.InvokeToken("1|toInt|addOne|addOne|addOne|asVar|b;"), 4);
             Assert.AreEqual(commandParser.InvokeToken("|useVar|b|addOne;"), 5);
@@ -105,12 +56,12 @@ namespace ExtractorSharp.UnitTest.Command
 
             Assert.AreEqual(commandParser.InvokeToken("|useVar|b.ToString()|Concat|aaaa;"), "6aaaa");
             Assert.IsTrue((
-                    commandParser.InvokeToken("|useVar|a|toInt|Concat|4|toInt|addOne|addOne;") as int[] ?? Array.Empty<int>()
+                    commandParser.InvokeToken("|useVar|a|toInt|Concat|4|toInt|addOne|addOne;") as int[] ?? new int[]{}
                 ).SequenceEqual(
                     new[] { 1, 2, 3, 6 }
                 ));
             Assert.IsFalse((
-                commandParser.InvokeToken("4|toInt|addOne|addOne|Concat|useVar|a|toInt;") as int[] ?? Array.Empty<int>()
+                commandParser.InvokeToken("4|toInt|addOne|addOne|Concat|useVar|a|toInt;") as int[] ?? new int[]{}
             ).SequenceEqual(
                 new[] { 6, 1, 2, 3 }
             )); // concat 左侧的值不应该因右侧的值改变类型.
